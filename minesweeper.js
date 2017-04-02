d4c9d0fcff2234e583ff261524caec9c22efe35e
Rob <superserva@gmail.com>
2017-04-02 16:43:27 +1200

    Functions to generate board and scatter mines compete

--- minesweeper.js
+++ minesweeper.js
@@ -1,65 +1,65 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-// Define your `board` object here!
+//Define your `board` object here!
 var board = {
-    cells: [
-        {
-            row: 0,
-            col: 0,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 0,
-            col: 1,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 0,
-            col: 2,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 1,
-            col: 0,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 1,
-            col: 1,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 1,
-            col: 2,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 2,
-            col: 0,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 2,
-            col: 1,
-            isMine: false,
-            hidden: true,
-            isMarked: false
-        }, {
-            row: 2,
-            col: 2,
-            isMine: true,
-            hidden: true,
-            isMarked: false
+    cells: []
+}
+
+var scatterMines = function(board) {
+    // This function take board object and randomly scatters mines into the board.
+
+    var mineCount = Math.ceil((15 / 100) * board.cells.length)
+    // Gets the number of mines that will be used on this sized board.
+    // 15 Percent of cells will be mines
+
+    for (var i = 0; i < mineCount; i++) {
+        // Choose a random cell on board
+        var mineIndex = Math.floor((Math.random() * board.cells.length) + 0);
+
+        // Make cell a mine
+        board.cells[mineIndex].isMine = true;
+    }
+
+}
+
+var generateBoard = function(cellNumber) {
+    // boardSize is the number of cells across and down
+    // confirm that cellNumber variable is a number and is divisiable by 2
+    if (!isNaN(cellNumber) && (cellNumber % 2 === 0) && (cellNumber > 4)) {
+        alert("You entered a valid number")
+        var boardSize = cellNumber
+
+        var r = 0
+        var c = 0
+
+        for (var r = 0; r < boardSize; r++) {
+            // Outer loop. Create all cells along row
+            var cell = {
+                row: r,
+                col: c,
+                isMine: false,
+                hidden: true,
+                isMarked: false
+            }
+            board.cells.push(cell);
+
+            // Inner loop. Create all cells down column
+            for (var c = 1; c < boardSize; c++) {
+                var cell = {
+                    row: r,
+                    col: c,
+                    isMine: false,
+                    hidden: true,
+                    isMarked: false
+                }
+                board.cells.push(cell);
+            }
         }
-    ]
-};
+    scatterMines(board);
+    } else {
+        alert('That is not a valid table size')
+    }
+}
 
 function startGame() {
 
@@ -107,23 +107,28 @@ function checkForWin() {
         //   lib.displayMessage('You win!')
     }
 }
-    // Define this function to count the number of mines around the cell
-    // (there could be as many as 8). You don't have to get the surrounding
-    // cells yourself! Just use `lib.getSurroundingCells`:
-    //
-    //   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-    //
-    // It will return cell objects in an array. You should loop through
-    // them, counting the number of times `cell.isMine` is true.
+// Define this function to count the number of mines around the cell
+// (there could be as many as 8). You don't have to get the surrounding
+// cells yourself! Just use `lib.getSurroundingCells`:
+//
+//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+//
+// It will return cell objects in an array. You should loop through
+// them, counting the number of times `cell.isMine` is true.
 
-    function countSurroundingMines(cell) {
-        var mines = []
-        var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
+function countSurroundingMines(cell) {
+    var mines = []
+    var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
 
-        for (var i = 0; i < surroundingCells.length; i++) {
-            if (surroundingCells[i].isMine === true) {
-                mines.push(surroundingCells[i]);
-            } else {}
-        }
-        return mines.length;
+    for (var i = 0; i < surroundingCells.length; i++) {
+        if (surroundingCells[i].isMine === true) {
+            mines.push(surroundingCells[i]);
+        } else {}
     }
+    return mines.length;
+}
+
+
+var UserInput = prompt('How cell across would you like your board? (must be bigger than four)');
+
+generateBoard(UserInput);
